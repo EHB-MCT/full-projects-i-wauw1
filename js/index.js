857dfec4ed78aa8d1eddf1781ac873be873d3077
Ilhan Kurultay <ilhan.kurultay@student.ehb.be>
2022-01-19 21:55:29 +0100

    webiste

--- js/index.js
+++ js/index.js
@@ -1,2 +1,13 @@
+function toonLijst() {
+    document.getElementById("lijstHamburger").style.visibility = 'visible';
+    document.getElementById("lijstHamburger").style.height = '200px';
+    document.getElementById("hamburger").style.display = 'none';
+    document.getElementById("x-nav-id").style.visibility = 'visible';
+}
 
-console.log("hello world")
+function sluitLijst() {
+    document.getElementById("lijstHamburger").style.visibility = 'hidden';
+    document.getElementById("lijstHamburger").style.height = '0px';
+    document.getElementById("hamburger").style.display = 'grid';
+    document.getElementById("x-nav-id").style.visibility = 'hidden';
+}
