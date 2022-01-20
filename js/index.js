ce42f639e8d6d1933705c553ba3d6f1380d15a08
Ilhan Kurultay <ilhan.kurultay@student.ehb.be>
2022-01-20 15:41:45 +0100

    changes

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ function toonLijst() {
     document.getElementById("lijstHamburger").style.height = '200px';
     document.getElementById("hamburger").style.display = 'none';
     document.getElementById("x-nav-id").style.visibility = 'visible';
+    document.getElementById("x-nav-id").style.display = 'grid';
 }
 
 function sluitLijst() {
@@ -10,4 +11,5 @@ function sluitLijst() {
     document.getElementById("lijstHamburger").style.height = '0px';
     document.getElementById("hamburger").style.display = 'grid';
     document.getElementById("x-nav-id").style.visibility = 'hidden';
+    document.getElementById("x-nav-id").style.display = 'none';
 }
