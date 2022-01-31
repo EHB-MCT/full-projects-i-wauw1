3ef2840b6055567bb62d296ba6c338415a0f94dd
Ilhan Kurultay <ilhan.kurultay@student.ehb.be>
2022-01-31 10:22:37 +0100

    v15

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
 function toonLijst() {
     document.getElementById("lijstHamburger").style.visibility = 'visible';
     document.getElementById("lijstHamburger").style.height = '200px';
+    document.getElementById("lijstHamburger").style.display = 'grid';
     document.getElementById("hamburger").style.display = 'none';
     document.getElementById("x-nav-id").style.visibility = 'visible';
     document.getElementById("x-nav-id").style.display = 'grid';
 }
 
 function sluitLijst() {
+    document.getElementById("lijstHamburger").style.display = 'none';
     document.getElementById("lijstHamburger").style.visibility = 'hidden';
     document.getElementById("lijstHamburger").style.height = '0px';
     document.getElementById("hamburger").style.display = 'grid';
@@ -35,12 +37,12 @@ function infoWorkshopsTwee() {
 }
 
 function homeCarousel() {
-    if (document.getElementById("homeWorkshop").style.display == "none") {
-        document.getElementById("homeOver").style.display = "none";
-        document.getElementById("homeWorkshop").style.display = "grid";
-    } else {
+    if (document.getElementById("homeWorkshop").style.display == "grid") {
         document.getElementById("homeOver").style.display = "grid";
         document.getElementById("homeWorkshop").style.display = "none";
+    } else {
+        document.getElementById("homeOver").style.display = "none";
+        document.getElementById("homeWorkshop").style.display = "grid";
     }
 }
 
