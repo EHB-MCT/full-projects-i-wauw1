1b46d4f9c7f432d0119da6fe71b8a534edd0b500
Ilhan Kurultay <ilhan.kurultay@student.ehb.be>
2022-02-02 10:19:15 +0100

    v43

--- js/index.js
+++ js/index.js
@@ -53,6 +53,28 @@ function log() {
     }
 }
 
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({
+        pageLanguage: 'nl',
+        autoDisplay: false,
+        includedLanguages: 'nl,fr,en',
+        layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+    }, 'google_translate_element');
+
+    function changeGoogleStyles() {
+        if ($('.goog-te-menu-frame').contents().find('.goog-te-menu2').length) {
+            $('.goog-te-menu-frame').contents().find('.goog-te-menu2').css({
+                'max-width': '100%',
+                'overflow-x': 'auto',
+                'box-sizing': 'border-box',
+                'height': 'auto'
+            });
+        } else {
+            setTimeout(changeGoogleStyles, 50);
+        }
+    }
+    changeGoogleStyles();
+}
 
 function homePijlte() {
     document.getElementById("architectId").style.display = "none";
